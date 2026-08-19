a06df3713687c2d2f23dd10a3c2cbffdd935d7d3
Bárbara Nivalda Palharini Alvim Sousa <barbara.palharini@uenp.edu.br>
2026-08-19 11:32:45 -0300

    Update API base URL

--- app/main.py
+++ app/main.py
@@ -29,8 +29,7 @@ from app.calculations.schemas import AvaliacaoCompletaInput, AvaliacaoCompletaRe
 ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "http://127.0.0.1:5174",
-    "http://localhost:5174",
+    "https://seu-projeto.vercel.app",
 ]
 
 
